8d86c94abae506b4b3a88c40118fedca97808760
dccurcija <dccurcija@lbl.gov>
2023-07-19 16:36:27 -0700

    This version is missing only Edge and Frame U-values.:

--- pyTHMX.py
+++ pyTHMX.py
@@ -1,49 +1,50 @@
 import os
 import xml.etree.ElementTree as ET
-import pandas as pd
+import csv
 
-def process_file(file_path):
-    # Parse XML directly from the file
+# This is the default namespace
+ns = {'lbl': 'http://windows.lbl.gov'}
+
+
+def extract_values_from_file(file_path):
+    # Parse the XML file
     tree = ET.parse(file_path)
     root = tree.getroot()
 
-    data = []
+    # Extract values
+    therm_version = root.find('lbl:ThermVersion', ns).text
+    cross_section_type = root.find('lbl:CrossSectionType', ns).text
+    frame_u_factor = None
+    edge_u_factor = None
+    for model in root.findall('.//lbl:Model', ns):
+        model_type = model.find('lbl:ModelType', ns).text
+        if model_type == 'U-factor':
+            for tag in model.findall('.//lbl:Tag', ns):
+                tag_text = tag.text
+                if tag_text == 'Frame':
+                    frame_u_factor = model.find('lbl:U-factor', ns).get('value')
+                elif tag_text == 'Edge':
+                    edge_u_factor = model.find('lbl:U-factor', ns).get('value')
 
-    # Iterate over all 'Case' elements
-    for case in root.findall('Case'):
-        case_name = case.find('Name').text
-        # Iterate over all 'U-factors' elements
-        for ufactors in case.findall('U-factors'):
-            tag = ufactors.find('Tag').text
-            if tag in ['Frame', 'Edge']:
-                delta_t = ufactors.find('DeltaT').attrib['value']
-                # Iterate over all 'Projection' elements
-                for projection in ufactors.findall('Projection'):
-                    length_type = projection.find('Length-type').text
-                    length = projection.find('Length').attrib['value']
-                    u_factor = projection.find('U-factor').attrib.get('value', 'NA')
-                    data.append([case_name, tag, delta_t, length_type, length, u_factor])
+    filename_without_ext = os.path.splitext(os.path.basename(file_path))[0]
 
-    # Create DataFrame
-    df = pd.DataFrame(data, columns=['Case Name', 'Tag', 'DeltaT', 'LengthType', 'Length', 'U-Factor'])
-    return df
+    return [therm_version, filename_without_ext, cross_section_type, frame_u_factor, edge_u_factor]
 
-# The top argument for walk (your directory path)
-topdir = 'THERM 8.0'
 
-# The arg argument for walk, and subsequently ext for step
-exten = '.thmx'
+def process_all_files(root_directory):
+    # Prepare the CSV file
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["ThermVersion", "Filename", "CrossSectionType", "Frame_U-factor", "Edge_U-factor"])
 
-dataframes = []
+        # Walk through root_directory
+        for dirpath, dirs, files in os.walk(root_directory):
+            for filename in files:
+                if filename.endswith('.thmx'):
+                    file_path = os.path.join(dirpath, filename)
+                    values = extract_values_from_file(file_path)
+                    writer.writerow(values)  # write to CSV file
 
-for dirpath, dirnames, files in os.walk(topdir):
-    for name in files:
-        if name.lower().endswith(exten):
-            df = process_file(os.path.join(dirpath, name))
-            dataframes.append(df)
 
-# Concatenate all dataframes
-final_df = pd.concat(dataframes)
-
-# Save to CSV
-final_df.to_csv('output.csv', index=False)
+# Then you can use the function with your root directory
+process_all_files('THERM 8.0')
